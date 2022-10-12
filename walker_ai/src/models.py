32023a046f860da0c21bf11a41d34dcfecd2f3d4
aihoque2 <aihoque2@illinois.edu>
2022-10-11 23:46:41 -0700

    add python code and new urdf

---
+++ walker_ai/src/models.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3
+
+"""
+models.py
+
+store our actor-critic models for the
+deep deterministic policy gradient
+"""
+
+import torch
+import numpy as np
+
+
+class Actor:
+    def __init__(self, input_size):
+        pass    
+    
+class Critic:
+    def __init__(self, input_size):
+        pass
